ee701b924f27072ebc50be48e7f4b0e99caf1041
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-12-15 20:22:24 -0300

    configurações iniciais

---
+++ css2/README.md
@@ -0,0 +1,12 @@
+# React Completo Origamid - Módulo CSS - <i> STYLED COMPONENTS: </i>
+
+## Definição:
+<p>
+
+
+## Ferramentas e Aprendizados:
+<li> </li>
+
+## Imagem do app:
+
+<img src="./img.PNG"/>
---
+++ css2/src/components/Produto.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Produto = () => {
+  return (
+    <div>
+      <h1>Notebook</h1>
+      <p>R$ 2.000,00</p>
+      <button>Comprar</button>
+
+      <p>De 4.000,00!!!</p>
+    </div>
+  );
+};
+
+export default Produto;
