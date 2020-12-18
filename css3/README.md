34456c892739ccd0f18f4900640cf7f7f57a8632
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-12-18 08:46:03 -0300

    REACT BOOTSTRAP

--- css3/README.md
+++ css3/README.md
@@ -1,15 +1,16 @@
 # React Completo Origamid - Módulo CSS - <i> CSS Frameworks: </i>
 
 <p>Podemos adicionar qualquer library/framework de css ao React. Com o @next vamos instalar a versão 5 do bootstrap. Popper é necessário para algumas funções do bootstrap.</p>
+
 ## 1.  Bootstrap:
 
-## Instalação:
+# Instalação:
 
-````
+```
 npm install bootstrap@next
-````
+```
 
-````
+```
 <!-- No index.js -->
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -25,6 +26,39 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
+```
+
+## 2. React  Bootstrap:
+<p>Existem frameworks de CSS que te fornecem componentes prontos para serem utilizados no local de classes. O react-bootstrap utiliza em sua base o bootstrap, mas fornece componentes React.</p>
+
+# Instalação:
+
+```
+npm install react-bootstrap bootstrap
+```
+
+```
+<!-- No index.js -->
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
+```
+
+```
+<!-- No app ou no componente: -->
+import React from 'react';
+import Card from 'react-bootstrap/Card';
 ```
 
 
--- css3/src/App.js
+++ css3/src/App.js
@@ -1,16 +1,39 @@
+// 1. BOOSTRAP:
+
+// import React from 'react';
+
+// const App = () => {
+//   return (
+//     <div className="card bg-dark text-white m-5" style={{ maxWidth: '18rem' }}>
+//       <div className="card-header">Notebook</div>
+//       <div className="card-body">
+//         <h5 className="card-title">R$ 2500</h5>
+//         <p className="card-text">
+//           Esse é um notebook com 1tb, 16gb de ram e placa de vídeo de 16gb.
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// 2. REACT BOOTSTRAP:
+
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 const App = () => {
   return (
-    <div className="card bg-dark text-white m-5" style={{ maxWidth: '18rem' }}>
-      <div className="card-header">Notebook</div>
-      <div className="card-body">
-        <h5 className="card-title">R$ 2500</h5>
-        <p className="card-text">
+    <Card bg="dark" text="white" style={{ maxWidth: '18rem' }} className="m-5">
+      <Card.Header>Notebook</Card.Header>
+      <Card.Body>
+        <Card.Title>R$ 2500</Card.Title>
+        <Card.Text>
           Esse é um notebook com 1tb, 16gb de ram e placa de vídeo de 16gb.
-        </p>
-      </div>
-    </div>
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
