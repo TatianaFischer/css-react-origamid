7ee3eaa66dd2de531fc85751b509145dd5b9c563
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-12-19 21:19:03 -0300

    animações e lógica

--- css5/src/components/Slide.js
+++ css5/src/components/Slide.js
@@ -2,9 +2,30 @@ import React from 'react';
 import styles from './Slide.module.css';
 
 const Slide = ({ slides }) => {
+  const [active, setActive] = React.useState(0);
+  const [position, setPosition] = React.useState(0);
+  const contentRef = React.useRef();
+
+  React.useEffect(() => {
+    const { width } = contentRef.current.getBoundingClientRect();
+    setPosition(-(width * active));
+  }, [active]);
+
+  function slidePrev() {
+    if (active > 0) setActive(active - 1);
+  }
+
+  function slideNext() {
+    if (active < slides.length - 1) setActive(active + 1);
+  }
+
   return (
     <section className={styles.container}>
-      <div className={styles.content}>
+      <div
+        ref={contentRef}
+        className={styles.content}
+        style={{ transform: `translateX(${position}px)` }}
+      >
         {slides.map((slide) => (
           <div key={slide.id} className={styles.item}>
             {slide.text}
@@ -12,8 +33,8 @@ const Slide = ({ slides }) => {
         ))}
       </div>
       <nav className={styles.nav}>
-        <button>Anterior</button>
-        <button>Próximo</button>
+        <button onClick={slidePrev}>Anterior</button>
+        <button onClick={slideNext}>Próximo</button>
       </nav>
     </section>
   );
