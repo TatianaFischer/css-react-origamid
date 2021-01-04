30e538abe7360984b29375229fd933fec448a956
tatianafischer <tatiana.fischer@acad.pucrs.br>
2021-01-04 08:56:54 -0300

    diversas formas de importat imagens, inclusive criando um novo componente

--- css5/src/App.js
+++ css5/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
+import DogSvg from './components/DogSvg';
 import Slide from './components/Slide';
+import dog from './img/dog.svg';
+import { ReactComponent as Dog } from './img/dog.svg';
 
 function App() {
   const slides = [
@@ -17,9 +20,26 @@ function App() {
       text: 'slide 3',
     },
   ];
+
+  const [raioOlho, setRaioOlho] = React.useState(2);
+
+  function handleClick() {
+    setRaioOlho(4);
+    for (let i = 0; i < 6; i++) {
+      setTimeout(() => {
+        setRaioOlho(i);
+      }, 20 * i);
+    }
+  }
   return (
     <>
       <Slide slides={slides} />
+      <p onClick={handleClick}>
+        <img src={dog} alt="dog" />
+        <Dog />
+        <DogSvg color="#84e" raioOlho="1" />
+        <DogSvg color="tomato" raioOlho={raioOlho} />
+      </p>
     </>
   );
 }
